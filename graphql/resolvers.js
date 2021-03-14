61f63e2111b404b4dd1eaaed527d3443c85d0f71
shj9206 <shj9206@gmail.com>
2021-03-14 10:11:20 +0900

    graphql final for education

---
+++ graphql/resolvers.js
@@ -0,0 +1,11 @@
+import {getMovies} from "./db"
+
+const resolvers = {
+    Query : {
+        movies: (_, { rating, limit }) => getMovies(limit, rating)
+    }
+  };
+  
+    
+
+export default resolvers;
